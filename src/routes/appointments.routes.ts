028ec18e07ae02f03ad2afb9644b675ad781b493
Felipe Silva <felipe.florianopolis@gmail.com>
2020-06-01 12:41:52 -0300

    Fist commit.

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+
+import { uuid } from 'uuidv4';
+
+const appointmentRouter = Router();
+
+const appointments = [];
+
+appointmentRouter.get('/', (request, response) => {
+  return response.json({ message: 'hello world' });
+});
+
+appointmentRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default appointmentRouter;
